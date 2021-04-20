8d957c38033146c45e8dd04ecd42c99452dfcbb8
JGuarin <julian.guarin@jayacompany.com>
2021-04-20 03:17:18 -0500

    Added Code for a MeshTexturedCube

--- Wolf/Sources/main.cpp
+++ Wolf/Sources/main.cpp
@@ -282,41 +282,42 @@ namespace DGE{
     /******************************************************************************************************************/
 
 // Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
-    enum Camera_Movement {
-        FORWARD,
-        BACKWARD,
-        LEFT,
-        RIGHT
-    };
 
-// Default camera values
+
+
+    /******************************************************************************************************************/
+    /** -- Camera Mouse/Keyboard -- **********************************************************************************/
+    /* An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL out of keyboard and mouse movement.
+     */
     const float YAW         = -90.0f;
     const float PITCH       =  0.0f;
-    const float SPEED       =  2.5f;
-    const float SENSITIVITY =  0.1f;
     const float ZOOM        =  45.0f;
-
-
-// An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
     class Camera
     {
     public:
-    // camera Attributes
+        enum class Camera_Movement {
+            FORWARD,
+            BACKWARD,
+            LEFT,
+            RIGHT
+        };
         glm::vec3 Position;
         glm::vec3 Front;
         glm::vec3 Up;
         glm::vec3 Right;
         glm::vec3 WorldUp;
-    // euler Angles
         float Yaw;
         float Pitch;
-    // camera options
-        float MovementSpeed;
-        float MouseSensitivity;
         float Zoom;
 
-    // constructor with vectors
-        Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+        Camera(
+            glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), 
+            glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), 
+            float yaw = DGE::YAW, 
+            float pitch = DGE::PITCH) 
+        : 
+        Front(glm::vec3(0.0f, 0.0f, -1.0f)),
+        Zoom(ZOOM)
         {
             Position = position;
             WorldUp = up;
@@ -324,8 +325,18 @@ namespace DGE{
             Pitch = pitch;
             updateCameraVectors();
         }
-    // constructor with scalar values
-        Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+        Camera(
+            float posX, 
+            float posY, 
+            float posZ, 
+            float upX, 
+            float upY, 
+            float upZ, 
+            float yaw, 
+            float pitch)
+        : 
+        Front(glm::vec3(0.0f, 0.0f, -1.0f)),
+        Zoom(ZOOM)
         {
             Position = glm::vec3(posX, posY, posZ);
             WorldUp = glm::vec3(upX, upY, upZ);
@@ -333,37 +344,88 @@ namespace DGE{
             Pitch = pitch;
             updateCameraVectors();
         }
-
-    // returns the view matrix calculated using Euler Angles and the LookAt Matrix
         glm::mat4 GetViewMatrix()
         {
             return glm::lookAt(Position, Position + Front, Up);
         }
 
-    // processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
-        void ProcessKeyboard(Camera_Movement direction, float deltaTime)
+    protected:
+        void updateCameraVectors()
         {
-            float velocity = MovementSpeed * deltaTime;
-            if (direction == FORWARD)
-                Position += Front * velocity;
-            if (direction == BACKWARD)
-                Position -= Front * velocity;
-            if (direction == LEFT)
-                Position -= Right * velocity;
-            if (direction == RIGHT)
-                Position += Right * velocity;
+            /* calculate the new 
+             * Front vector */
+            glm::vec3 front;
+            front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+            front.y = sin(glm::radians(Pitch));
+            front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+            Front = glm::normalize(front);
+            /* also re-calculate 
+             * the Right and Up vector */
+            Right = glm::normalize(glm::cross(Front, WorldUp));  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+            Up    = glm::normalize(glm::cross(Right, Front));
+        }
+    };
+    
+    const float MKBSPEED    =  2.5f;
+    const float MSENSITIVITY =  0.1f;
+    class Camera_MouseKeyboard : public Camera
+    {
+    public:
+        float MovementSpeed{ DGE::MKBSPEED };
+        float MouseSensitivity{ DGE::MSENSITIVITY };
+        
+        Camera_MouseKeyboard(
+            glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), 
+            glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), 
+            float yaw = YAW, 
+            float pitch = PITCH)
+        : 
+        Camera(position, up, yaw, pitch)
+        {
+
+        }
+        
+        Camera_MouseKeyboard(
+            float posX, 
+            float posY, 
+            float posZ, 
+            float upX, 
+            float upY, 
+            float upZ, 
+            float yaw, 
+            float pitch) 
+        :
+        Camera(posX, posY, posZ, upX, upY, upZ, yaw, pitch)
+        {
+
         }
 
-    // processes input received from a mouse input system. Expects the offset value in both the x and y direction.
+        void ProcessKeyboard(DGE::Camera::Camera_Movement direction, float deltaTime)
+        {
+            /* processes input received from any keyboard-like input system. 
+             * Accepts input parameter in the form of camera defined ENUM 
+             * (to abstract it from windowing systems) */
+            float displacement = MovementSpeed * deltaTime;
+            if (direction == DGE::Camera::Camera_Movement::FORWARD)
+                Position += Front * displacement;
+            if (direction == DGE::Camera::Camera_Movement::BACKWARD)
+                Position -= Front * displacement;
+            if (direction == DGE::Camera::Camera_Movement::LEFT)
+                Position -= Right * displacement;
+            if (direction == DGE::Camera::Camera_Movement::RIGHT)
+                Position += Right * displacement;
+        }
+
         void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true)
         {
+            /* processes input received from a mouse input system. Expects the offset value in 
+             * both the x and y direction. */
             xoffset *= MouseSensitivity;
             yoffset *= MouseSensitivity;
-
             Yaw   += xoffset;
             Pitch += yoffset;
 
-        // make sure that when pitch is out of bounds, screen doesn't get flipped
+            /* make sure that when pitch is out of bounds, screen doesn't get flipped */
             if (constrainPitch)
             {
                 if (Pitch > 89.0f)
@@ -372,13 +434,15 @@ namespace DGE{
                     Pitch = -89.0f;
             }
 
-        // update Front, Right and Up Vectors using the updated Euler angles
             updateCameraVectors();
         }
 
-    // processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
         void ProcessMouseScroll(float yoffset)
         {
+            /* processes input received from a 
+             * mouse scroll-wheel event. Only 
+             * requires input on the vertical 
+             * wheel-axis */
             Zoom -= (float)yoffset;
             if (Zoom < 1.0f)
                 Zoom = 1.0f;
@@ -386,23 +450,71 @@ namespace DGE{
                 Zoom = 45.0f; 
         }
 
-    private:
-    // calculates the front vector from the Camera's (updated) Euler Angles
-        void updateCameraVectors()
-        {
-        // calculate the new Front vector
-            glm::vec3 front;
-            front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-            front.y = sin(glm::radians(Pitch));
-            front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-            Front = glm::normalize(front);
-        // also re-calculate the Right and Up vector
-        Right = glm::normalize(glm::cross(Front, WorldUp));  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
-        Up    = glm::normalize(glm::cross(Right, Front));
-    }
-};
+    };
 
+    class MeshTextureCube {
+        std::vector<float> mVertexData {
+            -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+            0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+            0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+            0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+            -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+            0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+            0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+            0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+            0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+        };
+        unsigned int VBO;
+    public:
+        unsigned int VAO;
+        MeshTextureCube(){
+            glGenVertexArrays(1, &VAO);
+            glGenBuffers(1, &VBO);
+
+            glBindVertexArray(VAO);
+
+            glBindBuffer(GL_ARRAY_BUFFER, VBO);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(float) * mVertexData.size(), mVertexData.data() , GL_STATIC_DRAW);
+            // position attribute
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+            glEnableVertexAttribArray(0);
+            // texture coord attribute
+            glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+            glEnableVertexAttribArray(1);
+        }
+    };
 }
 
 
