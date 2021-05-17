3836c7b80b5dbf14dc4f7c0fb1b32f83d1bdd79b
JGuarin <julian.guarin@jayacompany.com>
2021-05-17 01:53:50 -0500

    Added Key types definitions

--- Wolf/Sources/dge/dgetypes.h
+++ Wolf/Sources/dge/dgetypes.h
@@ -1,3 +1,4 @@
+#pragma once
 namespace DGE {
 
     using iSize = glm::ivec2;
@@ -7,5 +8,36 @@ namespace DGE {
     using fColor3 = glm::vec3;
     using fLocation = glm::vec3;
     using xTextureID = unsigned int;
+    
+
+
+
+
+    enum class EventType{
+        EVENT_PRESSED,
+        EVENT_RELEASED
+    };
+    enum class KeyState{
+        STATE_DOWN,
+        STATE_UP,
+        STAKE_UNKNOWN
+    };
+
+    enum class Keys{
+        K_A,
+        K_S,
+        K_D,
+        K_W,
+        K_UP,
+        K_DWN,
+        K_LFT,
+        K_RGT,
+        K_SPC,
+        K_LCTL,
+        K_RCTL,
+        K_LALT,
+        K_RALT,
+        K_ESC
+    };
 
 }
